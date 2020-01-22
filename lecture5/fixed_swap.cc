ff04a87413d861a9a0a92781b88841b7ec5b522b
Zachary Ankenman <ankenman@gmail.com>
2020-01-21 18:30:09 -0800

    Adding files for lecture

---
+++ lecture5/fixed_swap.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int& x, int& y) {
+  int temp = x;
+  x = y;
+  y = temp;
+}
+
+int main() {
+  int firstNum = 10;
+  int secondNum = 20;
+
+  cout << "Before swap: " << "\n";
+  cout << firstNum << ", " << secondNum << "\n";
+
+  swap(firstNum, secondNum);
+
+  cout << "After swap: " << "\n";
+  cout << firstNum << ", " << secondNum << "\n";
+
+  return 0;
+}
+
+/*
+ * Code adapted from
+ * https://www.w3schools.com/cpp/cpp_function_param.asp
+ */
